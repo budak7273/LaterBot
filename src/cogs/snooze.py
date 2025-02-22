9b0569d7d60eed19a1a27990eea525b8d4b1cecf
Rob B <computerguy440+gh@gmail.com>
2025-02-22 17:47:44 -0500

    Database testing

--- src/cogs/snooze.py
+++ src/cogs/snooze.py
@@ -9,24 +9,23 @@ class Snooze(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @commands.user_command(
-        name="Remind me about this user",
-        contexts={
-            InteractionContextType.bot_dm,
-            InteractionContextType.private_channel,
-            InteractionContextType.guild,
-        },
-        integration_types={IntegrationType.guild_install, IntegrationType.user_install},
-    )
-    async def remind_user(
-        self, ctx: discord.ApplicationContext, member: discord.Member
-    ):
-        await ctx.respond(f"{ctx.author.mention} snoozing {member.mention}!")
+    # @commands.user_command(
+    #     name="Remind me about this user",
+    #     contexts={
+    #         InteractionContextType.bot_dm,
+    #         InteractionContextType.private_channel,
+    #         InteractionContextType.guild,
+    #     },
+    #     integration_types={IntegrationType.guild_install, IntegrationType.user_install},
+    # )
+    # async def remind_user(
+    #     self, ctx: discord.ApplicationContext, member: discord.Member
+    # ):
+    #     await ctx.respond(f"{ctx.author.mention} snoozing {member.mention}!")
 
     async def real_quick_snooze_message(
         self, ctx: discord.ApplicationContext, message: discord.Message
     ):
-
         current_utc_time = datetime.now(timezone.utc)
         remind_at = current_utc_time + timedelta(hours=6)
 
@@ -54,14 +53,14 @@ class Snooze(commands.Cog):
     ):
         await self.real_quick_snooze_message(ctx, message)
 
-    @commands.message_command(
-        name="DEBUG Quiz Snooze",
-        guild_ids=[1267160007854784633],
-    )
-    async def debug_quick_snooze_message(
-        self, ctx: discord.ApplicationContext, message: discord.Message
-    ):
-        await self.real_quick_snooze_message(ctx, message)
+    # @commands.message_command(
+    #     name="DEBUG Quiz Snooze",
+    #     guild_ids=[1267160007854784633],
+    # )
+    # async def debug_quick_snooze_message(
+    #     self, ctx: discord.ApplicationContext, message: discord.Message
+    # ):
+    #     await self.real_quick_snooze_message(ctx, message)
 
 
 def setup(bot: discord.Bot):
--- src/cogs/test.py
+++ src/cogs/test.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
-from ezcord import emb
+from ezcord import emb, log
+
+from db.db import db
 
 
 class Test(commands.Cog):
@@ -11,13 +13,25 @@ class Test(commands.Cog):
     async def greet(self, ctx: discord.ApplicationContext, member: discord.Member):
         await ctx.respond(f"{ctx.author.mention} says hello world to {member.mention}!")
 
-    @commands.message_command(name="Get Message ID")
+    @commands.user_command(name="Get DB Reminders")
+    async def get_user_reminders(
+        self, ctx: discord.ApplicationContext, member: discord.Member
+    ):
+        result = await db.get_users()
+        await emb.success(ctx, f"Result: {result}")
+
+    @commands.message_command(name="Get DB Row")
     async def get_message_id(
         self, ctx: discord.ApplicationContext, message: discord.Message
     ):
-        await emb.error(ctx, "Error message")
-        await emb.info(ctx, "Info message", title="Info Title")  # set an embed title
-        await ctx.respond(f"Message ID: `{message.id}`")
+        # await emb.error(ctx, "Error message")
+        # await emb.info(ctx, "Info message", title="Info Title")  # set an embed title
+        # await ctx.respond(f"Message ID: `{message.id}`")
+
+        await db.add_coins(12345, 100)
+        result = await db.get_one_user(12345)
+        log.info(str(result))  # (12345, 100)
+        await emb.success(ctx, f"Result: {result}")
 
 
 def setup(bot):
