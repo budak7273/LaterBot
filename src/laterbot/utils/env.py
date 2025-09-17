1a4b840af29ca232a2adf5f7a468b7dfc5223c56
Rob B <computerguy440+gh@gmail.com>
2025-09-17 19:32:04 -0400

    Also error if env vars are empty

--- src/laterbot/utils/env.py
+++ src/laterbot/utils/env.py
@@ -3,6 +3,6 @@ import os
 
 def get_env(key: str) -> str:
     val = os.getenv(key)
-    if val is None:
-        raise ValueError(f"Environment variable {key} is not set.")
+    if val is None or val is "":
+        raise ValueError(f"Environment variable '{key}' is not set or is empty string.")
     return val
