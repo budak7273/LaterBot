2c5d46dfaff042c1b44500df0af0451c9d0887c5
Rob B <computerguy440+gh@gmail.com>
2026-05-02 15:34:58 -0400

    fix string comparison

--- src/laterbot/utils/env.py
+++ src/laterbot/utils/env.py
@@ -3,6 +3,6 @@ import os
 
 def get_env(key: str) -> str:
     val = os.getenv(key)
-    if val is None or val is "":
+    if val == None or val == "":
         raise ValueError(f"Environment variable '{key}' is not set or is empty string.")
     return val
